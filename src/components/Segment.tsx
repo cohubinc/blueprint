850293eb0f0cc42c0ba47f96efa48d7535fdf935
Elliott Roche <eroche91@gmail.com>
2020-04-26 15:13:24 -0500

    removing default elevation

--- src/components/Segment.tsx
+++ src/components/Segment.tsx
@@ -37,7 +37,7 @@ export default function Segment(props: ISegmentProps) {
     padding = 2,
     bordered,
     borderColor,
-    elevation = "Primary",
+    elevation = 0,
     fullWidth = true,
   } = props;
 
