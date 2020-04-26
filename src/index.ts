738b5b94a5aa057bd39381a5fb533ab3ebbc5f48
Elliott Roche <eroche91@gmail.com>
2020-04-26 15:06:39 -0500

    added Segment component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Divider, IDividerProps } from "./components/Divider";
 export { default as Inline, IInlineProps } from "./components/Inline";
-export { default as Stack, IStackProps } from "./components/Stack";
-export { default as Spread, ISpreadProps } from "./components/Spread";
+export { default as Segment, ISegmentProps } from "./components/Segment";
 export { default as Space, ISpaceProps } from "./components/Space";
+export { default as Spread, ISpreadProps } from "./components/Spread";
+export { default as Stack, IStackProps } from "./components/Stack";
