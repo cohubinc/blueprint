88b3952843f0c84100588cdbc6285e8b5bccea47
Elliott Roche <eroche91@gmail.com>
2020-04-26 19:12:34 -0500

    adding width prop to Divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -4,18 +4,20 @@ export interface IDividerProps {
   marginSize?: 0.5 | 1 | 2 | 3 | 4 | 5 | 6;
   className?: string;
   color?: string;
+  width?: string;
 }
 
 export default function Divider({
   marginSize,
   className,
   color = "hsl(0, 0%, 92%)",
+  width = "100%",
 }: IDividerProps) {
   const style = {
     borderTop: `1px solid ${color}`,
     marginTop: `${marginSize || 0}rem`,
     marginBottom: `${marginSize || 0}rem`,
-    width: "100%",
+    width,
   };
 
   return <div className={className} style={style} />;
